e24d28f13c51a091b3ecb1167e5c87d702b96eb2
jmfavaro <jmfavaro@yahoo.fr>
2014-06-02 20:39:48 +0200

    premiere version en pas de 2000

---
+++ fusionIndMobPro_code.R
@@ -0,0 +1,53 @@
+getwd()
+setwd("D:/R/work")
+a<-ls()
+rm(all)
+a<-ls()
+library(RPostgreSQL)
+library(StatMatch)
+library(ade4)
+sessionInfo()
+getwd()
+setwd("D:/R/work")
+## configuration de la connection à PG
+dbDisconnect(con)
+dbUnloadDriver(m)
+
+m <- dbDriver("PostgreSQL")
+con <- dbConnect(m, user= "postgres", password="postgres", dbname="postgis21",  port=5434)
+##dbDisconnect(con)
+## don : table des donneurs
+for (i in 1:1919) {
+  print(i)
+  
+  query <- paste0("select agerevq, cs1, derou, dipl, empl, immi,inatc , lprm , moco, na5  , nperr, sexe , statr,  stocd, tp , trans, typl, typmr, voit, 1 as t, 'I'||ident as id, iris from fusion.fd_ind where idgeo=", i ," union select agerevq, cs1, derou, dipl, empl, immi, inatc , lprm , moco, na5  , nperr, sexe , statr,  stocd, tp , trans, typl, typmr, voit, 2 as t, 'M'||ident as id, 'XX' as iris from fusion.fd_mobpro where idgeo=", i ," ; ")
+  
+  fic <- dbGetQuery(con,query)
+  
+  identifiant <- fic$id ## identifiant des obs
+  
+  fic <- data.frame(lapply(fic[,1:22],factor)) ## transfo en facteur de toutes les variables
+  rownames(fic) <- identifiant
+  
+  afcm <- dudi.acm(df=fic[,1:19],scannf= FALSE,nf=100) ## acm sur les var agerev > voit
+  
+  coord <- afcm$li ##rownames est conserve
+  
+  
+  rec <- subset(coord, substr(rownames(coord),1,1)=='I') ##receveur : les iris
+  don <- subset(coord, substr(rownames(coord),1,1)=='M') ##donneur
+  
+  ## ajout d un id de 2000 en 2000
+  rec <- cbind(rec, 'id'= (c(1:nrow(rec)))) ## creation variable idex de 1 a nrow 
+  rec$id <- floor(rec$id / 2000) ## creation de 0 a xx
+  maxid <- max(rec$id)
+  ## boucle sur les paquets de 2000
+  for (j in 0:maxid) {
+    print(j)
+    out <- NND.hotdeck(data.rec=subset(rec,id==j), data.don=don, dist.fun="Euclidean",
+                       match.vars=colnames(don))
+    res <- data.frame(cbind(out$mtc.ids,out$dist.rd))
+    dbWriteTable(con, "resfusion", res, append = T)
+  }
+  rm(coord, afcm, fic, res, out)
+}
